e326af8563b7d05e13e504cf96e3c0e1faa7b93b
andrii <quadr988@gmail.com>
2023-06-16 22:28:00 +0300

    ht

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
-const URLS = [
-  "https://jira.ontrq.com/status",
-  "https://jira-stage.ontrq.com/status",
-  "https://confluence.ontrq.com/status",
-  "https://confluence-stage.ontrq.com/status",
-];
 
-fetch("https://jira.ontrq.com/status")
+const url = "https://us-east1-ac-monitors.cloudfunctions.net/ac-monitors-2"
+
+const root = document.querySelector(".root");
+
+fetch(url)
   .then((res) => res.json())
   .then((resp) => {
-    console.log(resp);
+    const data = resp;
+    const jiraProd = data["https://jira.ontrq.com/status"];
+    const jiraStage = data["https://jira-stage.ontrq.com/status"];
+    const conflStage = data["https://confluence.ontrq.com/status"];
+    const arr = [jiraProd, jiraStage, conflStage];
+    for (let instance of arr) {
+      const statusDiv = document.createElement("div");
+      statusDiv.innerHTML = `<div>
+          <p>${instance.state}</p>
+        </div>`;
+      root.appendChild(statusDiv);
+    }
+    
   });
+
+
