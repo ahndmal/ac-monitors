587e27e13608ab15e101751e0e126bc763e00f22
andrii <quadr988@gmail.com>
2023-06-17 16:41:53 +0300

    ht

--- index.js
+++ index.js
@@ -10,10 +10,13 @@ fetch(url)
     const jiraStage = resp[1]["jiraStage"];
     const conflStage = resp[2]["confProd"];
     const arr = [jiraProd, jiraStage, conflStage];
+    const state = instance === 'RUNNING' 
+      ? `<p style="color: green">${instance}</p>`
+      : `<p style="color: yellow">${instance}</p>`;
     for (let instance of arr) {
       const statusDiv = document.createElement("div");
       statusDiv.innerHTML = `<div>
-          <p>${instance}</p>
+          ${state}
         </div>`;
       root.appendChild(statusDiv);
     }
